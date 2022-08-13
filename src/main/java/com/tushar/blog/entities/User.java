fac43e5c091d7cc0372d7fc882f2e8cfa3304f03
tushar <tushargupta010@gmail.com>
2022-08-14 00:53:18 +0530

    Create User Entity

---
+++ src/main/java/com/tushar/blog/entities/User.java
@@ -0,0 +1,38 @@
+package com.tushar.blog.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private int id;
+	
+	@Column(name = "user_name", nullable = false, length = 100)
+	private String name;
+	
+	@Column(name = "user_email")
+	private String email;
+	
+	@Column(name = "user_password")
+	private String password;
+	
+	@Column(name = "user_about")
+	private String about;
+
+}
