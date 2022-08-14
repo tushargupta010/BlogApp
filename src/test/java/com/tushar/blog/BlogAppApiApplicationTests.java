9622b950cd5a39f27734038bfe8024a54253bf89
tushar <tushargupta010@gmail.com>
2022-08-14 12:51:29 +0530

    UserRepo implemented class test case

--- src/test/java/com/tushar/blog/BlogAppApiApplicationTests.java
+++ src/test/java/com/tushar/blog/BlogAppApiApplicationTests.java
@@ -1,13 +1,31 @@
 package com.tushar.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.tushar.blog.repository.UserRepo;
 
 @SpringBootTest
 class BlogAppApiApplicationTests {
 
+	@Autowired
+	private UserRepo userRepo;
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void userRepoTest() {
+		String userRepoImplementedClassName = this.userRepo.getClass().getName();
+		String userRepoImplementedClassPackageName = this.userRepo.getClass().getPackageName();
+		System.out.println("userRepoImplementedClassName : " + userRepoImplementedClassName);
+		System.out.println("userRepoImplementedClassPackageName : " + userRepoImplementedClassPackageName);
+		
+		// classes built at runtime for Repositories and we generally lnown as Proxy classes
+		// userRepoImplementedClassName : jdk.proxy2.$Proxy101
+		// userRepoImplementedClassPackageName : jdk.proxy2
 	}
 
 }
