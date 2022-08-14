4bef352a24a9329ff060f0cff05cd58d93d4cc0d
tushar <tushargupta010@gmail.com>
2022-08-14 16:01:10 +0530

    convert entity to entityDto and vice versa using ModelMapper

--- src/main/java/com/tushar/blog/BlogAppApiApplication.java
+++ src/main/java/com/tushar/blog/BlogAppApiApplication.java
@@ -1,7 +1,9 @@
 package com.tushar.blog;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogAppApiApplication {
@@ -10,4 +12,9 @@ public class BlogAppApiApplication {
 		SpringApplication.run(BlogAppApiApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
 }
