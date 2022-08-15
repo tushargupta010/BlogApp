47de55ae4d4b3518238edcd41227af0444da613d
tushar <tushargupta010@gmail.com>
2022-08-15 14:45:44 +0530

    post entity relationship with user and category

--- src/main/java/com/tushar/blog/entities/Post.java
+++ src/main/java/com/tushar/blog/entities/Post.java
@@ -1,15 +1,14 @@
 package com.tushar.blog.entities;
 
-import java.util.List;
+import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -17,22 +16,31 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "categories")
+@Table(name = "posts")
 @Getter
 @Setter
 @NoArgsConstructor
-public class Category {
+public class Post {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Integer postId;
 	
-	@Column(length = 100, nullable = false)
+	@Column(nullable = false)
 	private String title;
 	
-	private String description;
+	@Column(length = 10000)
+	private String content;
 	
-	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private List<Post> posts;
+	private String imageName;
+	
+	private Date addedDate;
+	
+	@ManyToOne
+	private Category category;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 	
 }
--- src/main/java/com/tushar/blog/entities/User.java
+++ src/main/java/com/tushar/blog/entities/User.java
@@ -1,10 +1,15 @@
 package com.tushar.blog.entities;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -35,4 +40,7 @@ public class User {
 	@Column(name = "user_about")
 	private String about;
 
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts;
+		
 }
